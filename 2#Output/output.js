0e7d2bcb654b2fc1aa776926a743e0413e99749c
masumwbdev <masumwbd@gmail.com>
2022-03-20 18:35:29 +0600

    output

---
+++ 2#Output/output.js
@@ -0,0 +1,14 @@
+console.log('hello js');
+
+function showMe(){
+    document.getElementById('demo').innerHTML = 33 + 22;
+}
+
+document.write('Hi this is document')
+
+window.alert('Hello this is window alert');
+alert('Hello this is only alert, no need window')
+
+function printMe(){
+    window.print()
+}
