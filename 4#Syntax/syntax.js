ad322a4b9c1febb356886fb25db0f7b1b4554ff3
masumwbdev <masumwbd@gmail.com>
2022-03-21 23:12:36 +0600

    syntax

---
+++ 4#Syntax/syntax.js
@@ -0,0 +1,52 @@
+// create variables
+var a;
+let b;
+
+// use variables
+c = 76;
+d = 98;
+let e = c - d;
+console.log(e);
+
+// js literals
+// number
+const numOne = 453.23;
+console.log(numOne);
+
+// strings
+const myName = "Masum";
+const mySurname = "Hossain";
+console.log(myName, mySurname)
+
+// js variables and assign value
+let myFav = "Mina";
+myFav = "Raju";
+console.log(myFav);
+
+// js operators
+const myNm = (56 - 54) * 2;
+console.log(myNm);
+
+// assignment operator uses
+let k , l;
+k = 7;
+l = 9;
+
+// js expressions
+const exp = 4 * 8;
+console.log(exp);
+
+// contain js exp variables
+const yt = 8;
+const ans = yt * 5;
+console.log(ans);
+
+// various types
+const vt = "Masum" + " " + "Hossain";
+console.log(vt);
+
+// js keywords
+let first, second;
+first = 8 + 9;
+second = first - 2;
+console.log(second);
