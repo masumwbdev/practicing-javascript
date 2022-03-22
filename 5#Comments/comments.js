65453d83ad6df3b9f27b7e15df5ebb7de9a04e35
masumwbdev <masumwbd@gmail.com>
2022-03-22 19:51:54 +0600

    comments

---
+++ 5#Comments/comments.js
@@ -0,0 +1,15 @@
+// single line comments
+// let a = "Apple";
+// a = "Orange";
+// console.log(a)
+const x = 5;
+const myName = "Masum";
+console.log(x, myName);
+
+// multiple line comments
+/*const handleSub = (e) => {
+    e.preventDefault();
+}*/
+
+const me = "hello my name is masum";
+console.log(me);
