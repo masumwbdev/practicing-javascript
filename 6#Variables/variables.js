3710b60a6f86d2bf021b122ba2217350c2aa4f90
masumwbdev <masumwbd@gmail.com>
2022-03-22 20:03:06 +0600

    js variables

---
+++ 6#Variables/variables.js
@@ -0,0 +1,12 @@
+// global variable (var)
+var x = "Masum";
+var myVar = "Dev";
+console.log(x, myVar)
+
+// es6 variables (const and let)
+const myNm = "Jack";
+console.log(myNm)
+
+let m = 76;
+m = 89;
+console.log(m)
