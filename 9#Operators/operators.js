2e60a91256fbd9413e711ec6258d8c0633916673
masumwbdev <masumwbd@gmail.com>
2022-03-24 20:24:23 +0600

    operators

---
+++ 9#Operators/operators.js
@@ -0,0 +1,64 @@
+let m = 89;
+let n = 78;
+let o = m + n;  // + is addition operator
+console.log(o)
+
+
+// = is assignment operator
+// assignment operator
+const fruits = 'apple';
+
+const ab = 7 - 3; // - is subtraction operator
+console.log(ab)
+
+const a = 8;
+const b = 4;
+const c = b * a;  // * is multiplication operator
+console.log(c)
+
+const x = 45 / 342;  // / is division operator
+console.log(x)
+
+// ++ is increment
+// -- is decrement
+// % is modulus 
+// ** is exponentiation operator
+
+// addition assignment operator +=
+let ac = 34;
+ac += 6;
+console.log(ac)
+
+// concatenate with addition operator
+const nOne = "Masum";
+const nTwo = "Mia";
+const res = nOne + " " + nTwo;
+console.log(res);
+
+// concate with +=
+let fOne = "Mr";
+fOne += "lichy";
+console.log(fOne);
+
+// string and number
+let ad = 4 + 5;
+let ae = "4" + 5;
+let af = "Masum" + 7;
+console.log(ad, ae, af);
+
+// ? is ternary operator
+
+const mat = 2;
+const resul = (8 + mat) * 4;
+const res2 = (8 * mat) + 4;
+console.log(resul);
+
+// exponentiation operator
+let xa = 4 ** 2;
+
+console.log(xa)
+
+const allRes = 10 + 5 * 3;
+const all2 = 10 + 5 - 3;
+const all3 = (10 + 5) * 3;
+console.log(allRes, all2, all3)
