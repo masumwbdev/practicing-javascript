d1fd169dc4f094b0931a935bb4bb5d2062eb86cb
masumwbdev <masumwbd@gmail.com>
2022-03-24 20:51:41 +0600

    arithmetic operator

---
+++ 10#Arithmetic-operator/arithmetic.js
@@ -0,0 +1,21 @@
+let x = 65 + 89;
+let y = 78 - 3;
+let z = 34 * 6;
+let a = 34 / 3;
+let b = 34 % 3;
+console.log(x, y, z, a, b);
+
+let ac = 4;
+let aa = (23 + 43) * ac;
+console.log(aa);
+
+let av = 7;
+av++;
+console.log(av)
+let atc = 7;
+atc++;
+console.log(atc)
+
+let ax = 5;
+let az = ax ** 2;
+console.log(az);
