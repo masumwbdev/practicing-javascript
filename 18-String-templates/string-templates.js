f03cbb6b7efb428ea0c2b3c3189bae2083ae975a
masumwbdev <masumwbd@gmail.com>
2022-04-19 21:54:06 +0600

    string templates

---
+++ 18-String-templates/string-templates.js
@@ -0,0 +1,17 @@
+const stringTmp = `Hello javascript`;
+const strTwo = `It's really beautiful`;
+const multStr = `
+    Hello everyone, How are you doing all.
+    Today I gonna tell you something about javascript programming. 
+    I think you are all ready to listen my lecture.
+    Javascript is a high-level programming language.
+    That's it for today. See you around.
+`;
+console.log(multStr);
+
+const firstName = "Md";
+const givenName = "Masum";
+const lastName = "Mia";
+const cityName = "Mymensingh";
+const helloMe = `Hi, my name is ${firstName} ${givenName} ${lastName}. I live in ${cityName}.`;
+console.log(helloMe);
