ead2c051674d78e7eb2e6b770d41aeb86aeac06f
masumwbdev <masumwbd@gmail.com>
2022-05-21 23:03:34 +0600

    iterate

--- 24-Array-iteration/iterate.js
+++ 24-Array-iteration/iterate.js
@@ -1,53 +1,53 @@
-// with forEach
-const numbers = [64, 23, 63, 21, 56, 93, 12];
-function myFunction(value, index, array) {
-    console.log(value);
-    console.log(index);
-    console.log(array);
-    console.log('------ break -------');
-}
-numbers.forEach(myFunction);
+// // with forEach
+// const numbers = [64, 23, 63, 21, 56, 93, 12];
+// function myFunction(value, index, array) {
+//     console.log(value);
+//     console.log(index);
+//     console.log(array);
+//     console.log('------ break -------');
+// }
+// numbers.forEach(myFunction);
 
-// with map
-const numbers = [64, 23, 63, 21, 56, 93, 12];
-function myFunction(value) {
-    return value * 2;
-}
-const newNum = numbers.map(myFunction);
-console.log(newNum);
+// // with map
+// const numbers = [64, 23, 63, 21, 56, 93, 12];
+// function myFunction(value) {
+//     return value * 2;
+// }
+// const newNum = numbers.map(myFunction);
+// console.log(newNum);
 
-// with filter
-const numbers = [64, 23, 63, 21, 56, 93, 12];
-function myFunction(value) {
-    return value > 50;
-}
-const newNum = numbers.filter(myFunction);
-console.log(newNum);
+// // with filter
+// const numbers = [64, 23, 63, 21, 56, 93, 12];
+// function myFunction(value) {
+//     return value > 50;
+// }
+// const newNum = numbers.filter(myFunction);
+// console.log(newNum);
 
-// with reduce
-const numbers = [64, 23, 63, 21, 56, 93, 12];
-function myFunction(total, value, index, array) {
-    console.log(total);
-    return total + value;
-}
-const newNum = numbers.reduce(myFunction , 20);
-console.log(newNum);
+// // with reduce
+// const numbers = [64, 23, 63, 21, 56, 93, 12];
+// function myFunction(total, value, index, array) {
+//     console.log(total);
+//     return total + value;
+// }
+// const newNum = numbers.reduce(myFunction , 20);
+// console.log(newNum);
 
-// with every
-const numbers = [64, 23, 63, 21, 56, 93, 12];
-function myFunction(value) {
-    return value > 13;
-}
-const newNum = numbers.every(myFunction);
-console.log(newNum);
+// // with every
+// const numbers = [64, 23, 63, 21, 56, 93, 12];
+// function myFunction(value) {
+//     return value > 13;
+// }
+// const newNum = numbers.every(myFunction);
+// console.log(newNum);
 
-// with some
-const numbers = [64, 23, 63, 21, 56, 93, 12];
-function myFunction(value) {
-    return value > 55;
-}
-const newNum = numbers.some(myFunction);
-console.log(newNum);
+// // with some
+// const numbers = [64, 23, 63, 21, 56, 93, 12];
+// function myFunction(value) {
+//     return value > 55;
+// }
+// const newNum = numbers.some(myFunction);
+// console.log(newNum);
 
 // indexOf
 const cars = ["Toyota", "Fort", "BMW"];
